b55b6a34c2177c0e864e71646c6042ad871b3393
Student <student@b528a-9.intranet.cs.upt.ro>
2026-04-22 11:21:27 +0300

    implemented list function

--- city_manager.c
+++ city_manager.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
 #define MANAGER 100007
 #define INSPECTOR 2100007
@@ -8,14 +11,32 @@ void add(char** line) {
 	return; 
 }
 
-void (*detect_command(char**))(char** line) {	
-	return (add); 
-}
+
 
 void list(char** line) {
-	int district_id = argv[6]; 
+	char filename[60]; 
+	snprintf(filename, sizeof(filename), "%s/%s", line[6], "reports.dat"); 	
+	int fd; 
+	if((fd = open(filename, O_RDONLY)) == -1) {
+		fprintf(stderr, "ERROR: you couldn't open the file!\n"); 
+		exit(EXIT_FAILURE); 
+	}
 
-	
+	char buffer[256]; 
+	size_t bytes_read;
+	while((bytes_read = read(fd, buffer, 256)) > 0) {
+		write(STDOUT_FILENO, buffer, bytes_read); 
+	}
+
+	close(fd); 
+}
+
+void (*detect_command(char** line))(char**) {	
+	if(strcmp(line[5], "--list") == 0) {
+		return list; 
+	}
+
+	return NULL; 
 }
 
 int main(int argc, char** argv) {
@@ -29,13 +50,13 @@ int main(int argc, char** argv) {
 	 -- argv[5] - command
 	 -- argv[6] - argument for the command  
 	*/
+
 	if(argc < 4) {
+		// write(stderr, "ERROR: you have not specified the role and the command!\n", 60); 
 		fprintf(stderr, "ERROR: you have not specified the role and the command!\n"); 
 		exit(EXIT_FAILURE);
 	}			
 
-	detect_command(argv);
-
+	list(argv); 
 	return 0; 
 }
-
